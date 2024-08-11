bb972675f23d17cca95b60816682603206a43d1e
L0osTing <alineoliveiraa02@hotmail.com>
2024-08-11 15:13:55 -0300

    first commit

---
+++ src/main/java/com/example/demo/controller/ProdutoController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Produto;
+import com.example.demo.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto) {
+        return repository.save(produto);
+    }
+}
